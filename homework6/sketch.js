5ff25c6d50f8cece5e733bbeeb422a8de3b01985
Beau Goldberg <beau.goldberg@umontana.edu>
2021-02-16 20:09:38 -0700

    Update sketch.js

--- homework6/sketch.js
+++ homework6/sketch.js
@@ -21,7 +21,9 @@ function setup() {
     characterObjects.addAnimation('jump',jump[0],jump[jump.length-1]);
     
     cat = createSprite(100,680);
+    branches = createSprite(1400,100);
     cat.addImage(loadImage('assets/extra (1).png'));
+    branches.addImage(loadImage('assets/extra (2).png'));
 }
 
 // display all the frames using the draw function as a loop
@@ -34,22 +36,34 @@ function draw() {
     if (keyDown('a')) { 
         characterObjects.changeAnimation('walk_rv'); 
         characterObjects.velocity.x -= .2;
-        if(characterObjects.collide(cat)) {
+        if(characterObjects.collide(cat) || characterObjects.collide(branches))  {
             characterObjects.changeAnimation('idle');
         }
     }
     else if (keyDown('d')) { 
         characterObjects.changeAnimation('walk'); 
         characterObjects.velocity.x += .2;
+        if(characterObjects.collide(cat) || characterObjects.collide(branches))  {
+            characterObjects.changeAnimation('idle');
+        }
     }
     else if (keyDown('w')) {
         characterObjects.changeAnimation('jump');
-        //characterObjects.velocity.y -= .1;
+        characterObjects.velocity.y -= .1;
+        if(characterObjects.collide(cat) || characterObjects.collide(branches))  {
+            characterObjects.changeAnimation('idle');
+        }
+    }
+    else if (keyDown('s')) {
+        characterObjects.velocity.y += .1;
+        if(characterObjects.collide(cat) || characterObjects.collide(branches))  {
+            characterObjects.changeAnimation('idle');
+        }
     }
     else { 
         characterObjects.changeAnimation('idle'); 
         characterObjects.velocity.x = 0;
-        //characterObjects.velocity.y = 0;
+        characterObjects.velocity.y = 0;
     }
 
     if (characterObjects.position.x - 300 >= 1800) {
@@ -58,6 +72,7 @@ function draw() {
     }
 
     characterObjects.debug = mouseIsPressed;
+    branches.debug = mouseIsPressed;
     cat.debug = mouseIsPressed;
     drawSprites();
 }
